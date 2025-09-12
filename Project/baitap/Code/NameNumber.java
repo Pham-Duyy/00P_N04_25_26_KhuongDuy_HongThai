2a2a3c141133dea943a5c0d648389bc076d07d35
Phạm Khương Duy <binmarket021@gmail.com>
2025-09-12 07:19:54 +0000

    updating

---
+++ Project/baitap/Code/NameNumber.java
@@ -0,0 +1,21 @@
+
+public class NameNumber {
+    private String lastName;
+    private String telNumber;
+
+    // Constructor
+    public NameNumber(String lastName, String telNumber) {
+        this.lastName = lastName;
+        this.telNumber = telNumber;
+    }
+
+    // Getter for lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Getter for telNumber
+    public String getTelNumber() {
+        return telNumber;
+    }
+}
