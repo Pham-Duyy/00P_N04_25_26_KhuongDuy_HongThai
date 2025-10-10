1e47ba99cedf3b66dd91fe3bffad3cccd0343b79
Phạm Khương Duy <binmarket021@gmail.com>
2025-10-10 07:59:32 +0000

    update threads

--- gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Threadexample.java
+++ gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/Threadexample.java
@@ -1,6 +1,6 @@
 package com.example.servingwebcontent;
 
-public class threadexample extends Thread {
+public class Threadexample extends Thread {
     public static int count = 0;
 
     public static void main(String[] args) throws InterruptedException {
