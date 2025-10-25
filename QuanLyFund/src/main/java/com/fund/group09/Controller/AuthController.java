3dad364d63f7d472256d84ef74bb8c55d66b6864
Pham Duy <binmarket021@gmail.com>
2025-10-25 19:59:52 +0700

    updating

---
+++ QuanLyFund/src/main/java/com/fund/group09/Controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.fund.group09.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login") 
+    public String loginPage() {
+        return "users/login"; // Thêm users/ vào trước
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "users/register"; // Thêm users/ vào trước
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboardPage() {
+        return "dashboard";
+    }
+}
