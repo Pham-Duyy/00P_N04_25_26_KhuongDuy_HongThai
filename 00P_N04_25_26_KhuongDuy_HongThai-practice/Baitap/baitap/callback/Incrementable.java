b5bf67815fdb47f7f968aa2cb26d7dd761092e32
WokHuy <23010184@st.phenikaa-uni.edu.vn>
2025-10-30 16:43:01 +0000

    duy mượn acc

--- 00P_N04_25_26_KhuongDuy_HongThai-practice/Baitap/baitap/callback/Incrementable.java
+++
@@ -1,5 +0,0 @@
-package callback;
-
-public interface Incrementable {
-    void increment();
-}
--- 00P_N04_25_26_KhuongDuy_HongThai-practice/Baitap/src/test/testRoleList.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class testRoleList {
-    public static void main(String[] args) {
-        System.out.println("Test RoleList class");
-    }
-    
-}
